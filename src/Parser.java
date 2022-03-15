05ff6bea040d9b894858a42ef3d59f2389c832f5
SophieLee <leesb19@wfu.edu>
2022-03-15 11:05:16 -0400

    started parsing

--- src/Parser.java
+++ src/Parser.java
@@ -27,6 +27,22 @@ public class Parser {
         which enlarge the size of file
          */
         RandomAccessFile ifrd = new RandomAccessFile(infoToParse, "r");
+
+        //wrap this in some sort of loop
+        int position = ifrd.readInt();
+        int length = ifrd.readInt();
+
+        //use position and length to add first block to input buffer (where to create input buffer?)
+        // then repeat !
+
+        byte[] inputBuff = new byte[8192]; // "a block is 8,192 bytes" according to project description
+
+        raf.readFully(inputBuff, position, length);
+        //now: add this block to RAM minheap
+
+        //then: use seek to start getting next block from Run file
+        long currFilePointer = position + length; //would this need to be manually converted to long?
+        raf.seek(currFilePointer);
     }
 
     public void run(){
